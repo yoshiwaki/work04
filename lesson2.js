1beb4603f6a0c1f43837e0b314c42caec5f66899
yoshiwaki <wakk12107@au.com>
2021-10-10 00:48:15 +0900

    オブジェクトを使って機能追加（未完成）

--- lesson2.js
+++ lesson2.js
@@ -1,15 +1,15 @@
 const button = document.getElementById('button');
 button.addEventListener('click',()=>{
   const message = document.getElementById('message');
-  const list = document.getElementById('list');
-  const lis = document.createElement('li');
-  const work = document.createElement('button');
-  const del = document.createElement('button');
-  
-  lis.innerHTML = message.value;
-  work.innerHTML = "作業中";
-  del.innerHTML = "削除";
-  list.appendChild(lis);
-  lis.appendChild(work);
-  lis.appendChild(del);
+  const task = {title: message.value,status:'作業中',delete:'削除'};
+  const tasks = [];
+  tasks.push(task);
+  console.log(tasks);
+
+     for (const tas of tasks) {
+      const list = document.getElementById('list');
+      const lis = document.createElement('li');
+       lis.innerHTML = tas;
+       list.appendChild(lis);
+     }
 })
