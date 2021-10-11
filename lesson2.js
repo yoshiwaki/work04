7cf3d57516309097ef6b02e0b2e37e131d3a09ac
yoshiwaki <wakk12107@au.com>
2021-10-11 14:52:39 +0900

    削除機能の実装（未完成）

--- lesson2.js
+++ lesson2.js
@@ -1,15 +1,58 @@
-const button = document.getElementById('button');
-button.addEventListener('click',()=>{
-  const message = document.getElementById('message');
-  const task = {title: message.value,status:'作業中',delete:'削除'};
-  const tasks = [];
-  tasks.push(task);
-  console.log(tasks);
+'use strict';
+const taskTrigger = document.getElementById('task-trigger');
+const taskValue = document.getElementById('task-value');
+const taskTable = document.getElementById('table');
+const tasks = [];
 
-     for (const tas of tasks) {
-      const list = document.getElementById('list');
-      const lis = document.createElement('li');
-       lis.innerHTML = tas;
-       list.appendChild(lis);
-     }
-})
+const createStatusButton = (task, status) => {
+    const statusButton = document.createElement('button');
+    statusButton.innerText = task.status;
+    status.appendChild(statusButton);
+}
+
+const createRemoveButton = remove => {
+    const removeButton = document.createElement('button');
+    removeButton.innerText = '削除';
+    remove.appendChild(removeButton);
+}
+
+
+const showTasks = () => {
+  taskTable.innerText = '';
+  tasks.forEach((task) => {
+    const taskId = taskTable.rows.length;
+    
+    const row = taskTable.insertRow(-1);
+
+  
+    const id = row.insertCell(0);
+    const comment = row.insertCell(1);
+    const status = row.insertCell(2);
+    const remove = row.insertCell(3);
+  
+    id.innerText = taskId;
+    comment.innerText = task.task;
+    createStatusButton(task, status);
+    createRemoveButton(remove);
+
+    remove.addEventListener('click',() =>{
+      while(taskId>0)taskTable.deleteRow(0);
+      tasks.shift(taskId)
+      showTasks(tasks)
+    })
+  });
+}
+
+
+taskTrigger.addEventListener('click', () => {
+  const task = { task: taskValue.value, status: '作業中' }
+  tasks.push(task);
+  showTasks();
+  taskValue.value = '';
+});
+
+
+
+
+
+
