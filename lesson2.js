3db11ea51302ccd4825fc1336fa3ae70579b2add
yoshiwaki <wakk12107@au.com>
2021-10-17 02:38:24 +0900

    教えていただいた削除機能と３の３の機能実装

--- lesson2.js
+++ lesson2.js
@@ -2,64 +2,60 @@
 const taskTrigger = document.getElementById('task-trigger');
 const taskValue = document.getElementById('task-value');
 const taskTable = document.getElementById('table');
-const tasks = [];
+const todos = [];
 
-const createStatusButton = (task, status) => {
+const createStatusButton = (todo,status,row) => {
     const statusButton = document.createElement('button');
-    statusButton.innerText = task.status;
+    statusButton.innerText = todo.status;
     status.appendChild(statusButton);
+  statusButton.addEventListener('click',()=>{
+
+    if(todo.status === '完了'){
+      todo.status = '作業中';
+      showTasks();
+    }else{
+    todo.status = "完了";
+   showTasks();
+    }
+  })
 }
-
-const createRemoveButton = remove => {
-    const removeButton = document.createElement('button');
-    removeButton.innerText = '削除';
-    remove.appendChild(removeButton);
-    remove.addEventListener('click',()=>{
-      // const taskId = taskTable.rows.length;
-      // console.log(taskId)
-      // const id = taskId.innerText
-      // console.log(id)
-      taskTable.deleteRow(Object.keys(tasks))
-    
-      
-      tasks.splice(Object.keys(tasks));
-      
-        showTasks();
-      })
+const createRemoveButton = (remove,row) => {
+  const removeButton = document.createElement('button');
+  removeButton.innerText = '削除';
+  remove.appendChild(removeButton);
+  removeButton.addEventListener('click',()=>{
+    const index = row.rowIndex - 1;
+    todos.splice(index,1);
+      showTasks();
+  })
 }
-
-
 const showTasks = () => {
   taskTable.innerText = '';
-  tasks.forEach((task) => {
+  todos.forEach((todo) => {
     const taskId = taskTable.rows.length;
-    
     const row = taskTable.insertRow(-1);
-
-  
+    // row.classList.add('tasks')
+    // if (todo.status === '完了') {
+    //   row.classList.add('finish');
+    // }
     const id = row.insertCell(0);
     const comment = row.insertCell(1);
     const status = row.insertCell(2);
     const remove = row.insertCell(3);
   
     id.innerText = taskId;
-    comment.innerText = task.task;
-    createStatusButton(task, status);
-    createRemoveButton(remove);
+    comment.innerText = todo.task;
+    createStatusButton(todo,status,row);
+    createRemoveButton(remove,row);
     
   })
   
 }
-
 taskTrigger.addEventListener('click', () => {
   const task = { task: taskValue.value, status: '作業中' }
-  tasks.push(task);
+  todos.push(task);
   showTasks();
   taskValue.value = '';
-
-  
-  
-  
 })
 
 
